6315600fd96ac8faa62963c8e89615e5f6f865a0
Alexey Rybakov <alexrybakov@yandex.ru>
2020-08-01 17:08:24 +0300

    92. Component Update Lifecycle (for state Changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import classes from './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[App.js] constructor');
+    }
+
     state = {
         persons: [
             {
@@ -25,6 +30,24 @@ class App extends Component {
             }
         ],
         showPersons: false
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log('[App.js] getDerivedStateFromProps', props);
+        return state;
+    }
+
+    componentDidMount() {
+        console.log('[App.js] componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[App.js] shouldComponentUpdate');
+        return true;
+    }
+
+    componentDidUpdate() {
+        console.log('[App.js] componentDidUpdate');
     }
 
     deletePersonHandler = (personIndex) => {
@@ -53,6 +76,7 @@ class App extends Component {
     }
 
     render() {
+        console.log('[App.js] render');
         let persons = null;
 
         if (this.state.showPersons) {
@@ -67,7 +91,8 @@ class App extends Component {
                 <Cockpit
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
-                    clicked={this.togglePersonsHandler} />
+                    clicked={this.togglePersonsHandler}
+                    title={this.props.appTitle} />
                 {persons}
             </div>
         );
