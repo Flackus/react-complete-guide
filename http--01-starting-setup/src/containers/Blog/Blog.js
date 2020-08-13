bfbc690f3f189c9131e18ff376235f32b0fe95d4
Alexey Rybakov <alexrybakov@yandex.ru>
2020-08-13 11:26:55 +0300

    176. Useful Resources & Links

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -9,12 +9,13 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false,
     }
 
     componentDidMount() {
         axios
-            .get('http://jsonplaceholder.typicode.com/posts')
+            .get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -24,6 +25,9 @@ class Blog extends Component {
                     };
                 });
                 this.setState({posts: updatedPosts});
+            })
+            .catch((_) => {
+                this.setState({error: true});
             });
     }
 
@@ -32,13 +36,17 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map((post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => {this.postSelectedHandler(post.id)}} />;
-        }));
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong...</p>;
+
+        if (!this.state.error) {
+            posts = this.state.posts.map((post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => {this.postSelectedHandler(post.id)}} />;
+            }));
+        }
 
         return (
             <div>
--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    // Edit request config
+    return requestConfig;
+}, error => {
+    console.error('Something is broken', error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+    // Edit response config
+    return responseConfig;
+}, error => {
+    console.error('Something is broken', error);
+    return Promise.reject(error);
+});
+
+ReactDOM.render(<App />, document.getElementById('root'));
