d5b0338da569fdc43595b62feb2a407df8c3cf73
Alexey Rybakov <alexrybakov@yandex.ru>
2020-09-09 11:27:47 +0300

    268. Understanding State Types

--- redux/src/containers/Counter/Counter.js
+++ redux/src/containers/Counter/Counter.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
     state = {
@@ -30,22 +32,49 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl
                     label="Increment"
-                    clicked={() => this.counterChangedHandler('inc')} />
+                    clicked={this.props.onIncrementCounter} />
                 <CounterControl
                     label="Decrement"
-                    clicked={() => this.counterChangedHandler('dec')} />
+                    clicked={this.props.onDecrementCounter} />
                 <CounterControl
-                    label="Add 5"
-                    clicked={() => this.counterChangedHandler('add', 5)} />
+                    label="Add 10"
+                    clicked={this.props.onAddCounter} />
                 <CounterControl
-                    label="Subtract 5"
-                    clicked={() => this.counterChangedHandler('sub', 5)} />
+                    label="Subtract 15"
+                    clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store result</button>
+                <ul>
+                    {this.props.storedResults.map(res => (
+                        <li
+                            key={res.id}
+                            onClick={() => this.props.onDeleteResult(res.id)}>{res.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+        onAddCounter: () => dispatch({ type: actionTypes.ADD, val: 10 }),
+        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, val: 15 }),
+        onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result }),
+        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, id })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ redux/src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: Date.now().toString(),
+                    value: action.result
+                })
+            };
+        case actionTypes.DELETE_RESULT:
+            const updatedArray = state.results.filter(el => el.id !== action.id);
+            return {
+                ...state,
+                results: updatedArray
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
